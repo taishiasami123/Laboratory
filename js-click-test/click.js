3b669646e9d00e8636c7b062bc9f8d773884a8c1
taishiasami123 <taishi.asami123@gmail.com>
2020-02-25 11:12:21 +0900

    アコーディオンテスト

---
+++ js-click-test/click.js
@@ -0,0 +1,8 @@
+const check = document.getElementById("mycheck");
+const text = document.getElementById("mytext");
+const arrow = document.getElementById("myarrow")
+
+check.onclick = () => {
+    text.classList.toggle("isShow");
+    arrow.classList.toggle("isShow");
+}
