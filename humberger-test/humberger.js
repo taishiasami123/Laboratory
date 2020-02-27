ee52de595181cb49274cc30c16dbf254f99e502a
taishiasami123 <taishi.asami123@gmail.com>
2020-02-27 19:00:57 +0900

    ハンバーガーメニューが開いた時に下の要素が動いてしまうのを修正

--- humberger-test/humberger.js
+++ humberger-test/humberger.js
@@ -1,14 +1,12 @@
 const humberger = document.getElementById("humberger");
 const line = document.getElementsByClassName("line");
-const item = document.getElementsByClassName("header-nav-list-item")
-const kv = document.getElementsByClassName("key-visual")
+const item = document.getElementsByClassName("header-nav-list-item");
 
 humberger.onclick = () => {
     for (let i = 0; i < line.length; i++) {
         line[i].classList.toggle("active");
-    }
+    };
     for (let i = 0; i < item.length; i++) {
         item[i].classList.toggle("open");
-    }
-    kv[0].classList.toggle("open")
-}
+    };
+}; 
