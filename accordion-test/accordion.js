161bfc47200783edd879f0296c72e528a8626098
taishiasami123 <taishi.asami123@gmail.com>
2020-02-28 14:42:17 +0900

    onclickする要素を2つにするテスト

--- accordion-test/accordion.js
+++ accordion-test/accordion.js
@@ -1,8 +1,17 @@
-const check = document.getElementById("mycheck");
-const text = document.getElementById("mytext");
-const arrow = document.getElementById("myarrow");
+const check1 = document.getElementById("check1");
+const text1 = document.getElementById("text1");
+const arrow1 = document.getElementById("arrow1");
 
-check.onclick = () => {
-    text.classList.toggle("isShow");
-    arrow.classList.toggle("isShow");
-}
+check1.onclick = () => {
+    text1.classList.toggle("open");
+    arrow1.classList.toggle("open");
+};
+
+const check2 = document.getElementById("check2");
+const text2 = document.getElementById("text2");
+const arrow2 = document.getElementById("arrow2");
+
+check2.onclick = () => {
+    text2.classList.toggle("open");
+    arrow2.classList.toggle("open");
+};
